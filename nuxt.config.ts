d4d5b7433186933b221c9157678a6e54d1ad841d
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-01 11:48:15 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,6 @@ export default defineNuxtConfig({
     storageBucket: process.env.NUXT_STORAGE_BUCKET,
     messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID,
     appId: process.env.NUXT_APP_ID,
-    measurementId: process.env.NUXT_MEASUREMENT_ID,
     public: {
       apiKey: process.env.NUXT_API_KEY_TEST,
       authDomain: process.env.NUXT_AUTH_DOMAIN_TEST,
