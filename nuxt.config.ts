4ec4228ca7364b95321d88224d749c204a60bd19
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-01 11:29:42 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,13 +17,12 @@ export default defineNuxtConfig({
     appId: process.env.NUXT_APP_ID,
     measurementId: process.env.NUXT_MEASUREMENT_ID,
     public: {
-      apiKey: process.env.NUXT_API_KEY,
-      authDomain: process.env.NUXT_AUTH_DOMAIN,
-      projectId: process.env.NUXT_PROJECT_ID,
-      storageBucket: process.env.NUXT_STORAGE_BUCKET,
-      messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID,
-      appId: process.env.NUXT_APP_ID,
-      measurementId: process.env.NUXT_MEASUREMENT_ID,
+      apiKey: process.env.NUXT_API_KEY_TEST,
+      authDomain: process.env.NUXT_AUTH_DOMAIN_TEST,
+      projectId: process.env.NUXT_PROJECT_ID_TEST,
+      storageBucket: process.env.NUXT_STORAGE_BUCKET_TEST,
+      messagingSenderId: process.env.NUXT_MESSAGING_SENDER_ID_TEST,
+      appId: process.env.NUXT_APP_ID_TEST,
     }
   },
 });
