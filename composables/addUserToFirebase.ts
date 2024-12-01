dd65deb18f7f0927c96a802e470f300b8aea8946
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-01 12:19:14 -0300

    Update addUserToFirebase.ts

--- composables/addUserToFirebase.ts
+++ composables/addUserToFirebase.ts
@@ -1,15 +1,15 @@
-import { collection, getFirestore, addDoc } from "firebase/firestore";
+import { setDoc, doc, getFirestore } from "firebase/firestore";
 
 export default async function addUserToFirebase(userData: any) {
   try {
     const db = getFirestore();
-    const usersCollection = collection(db, "users");
-    const docRef = await addDoc(usersCollection, userData);
+    const userDocRef = doc(db, "users", userData.uid);  // Use o uid como ID do documento
+    await setDoc(userDocRef, userData);  // Cria ou atualiza o documento com os dados
 
-    console.log("Usuário adicionado com ID: ", docRef.id);
-    return true
+    console.log("Usuário adicionado com ID: ", userData.uid);
+    return true;
   } catch (error) {
     console.error("Erro ao adicionar usuário: ", error);
-    return false
+    return false;
   }
 }
