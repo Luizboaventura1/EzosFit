fffd7aa75d977b002f6eaed71d1dea939bde4be8
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-12 18:04:09 -0300

    Update firebase.client.ts

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -5,25 +5,23 @@ import { getFirestore } from "firebase/firestore";
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
 
-    const firebaseConfig = {
-      apiKey: config.public.apiKey as string,
-      authDomain: config.public.authDomain as string,
-      projectId: config.public.projectId as string,
-      storageBucket: config.public.storageBucket as string,
-      messagingSenderId: config.public.messagingSenderId as string,
-      appId: config.public.appId as string,
-      measurementId: config.public.measurementId as string,
-    };
+  const firebaseConfig = {
+    apiKey: config.public.apiKey as string,
+    authDomain: config.public.authDomain as string,
+    projectId: config.public.projectId as string,
+    storageBucket: config.public.storageBucket as string,
+    messagingSenderId: config.public.messagingSenderId as string,
+    appId: config.public.appId as string,
+    measurementId: config.public.measurementId as string,
+  };
 
-    const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
-    const auth = getAuth(app);
-    const firestore = getFirestore(app);
+  const auth = getAuth(app);
+  const firestore = getFirestore(app);
 
-    nuxtApp.vueApp.provide("auth", auth);
-    nuxtApp.provide("auth", auth);
-
-    nuxtApp.vueApp.provide("firestore", firestore);
-    nuxtApp.provide("firestore", firestore);
-
+  nuxtApp.vueApp.provide("auth", auth);
+  nuxtApp.provide("auth", auth);
+  nuxtApp.vueApp.provide("firestore", firestore);
+  nuxtApp.provide("firestore", firestore);
 });
