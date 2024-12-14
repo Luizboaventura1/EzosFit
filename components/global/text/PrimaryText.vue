3f9d4f17a5812aef65b7de531fa7cabcc671f3fe
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-14 12:06:39 -0300

    Add PrimaryText component

---
+++ components/global/text/PrimaryText.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 :class="font" class="text-light-gray font-semibold">
+    <slot />
+  </h1>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  sm?: boolean;
+  md?: boolean;
+  lg?: boolean;
+  xl?: boolean;
+}>();
+
+const font = computed(() => {
+  if (props.sm) return "text-sm";
+  if (props.md) return "text-base";
+  if (props.lg) return "text-lg";
+  if (props.xl) return "text-xl";
+  return "";
+});
+</script>
