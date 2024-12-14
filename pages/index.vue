a748b425fdb2f1a3440d7b426ebd7a4ddce297b9
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-14 12:04:23 -0300

    Add home

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-dark-charcoal h-screen flex justify-center items-center px-10">
+    <h1 class="text-light-gray text-lg">Em fase de desenvolvimento 🚧</h1>
+  </div>  
+</template>
+
+<script setup lang="ts">
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+onMounted(() => {
+  const auth = getAuth();
+  
+  onAuthStateChanged(auth, (user) => {
+    let token = useCookie<boolean>("token");
+    token.value = !!user;
+
+    if (token.value) {
+      router.push("/dashboard");
+    }
+  });
+});
+</script>
+
+<style scoped></style>
