1695109acfcd26528cf533781beeaa907f27d179
Luiz Henrique <luizboaventura24@gmail.com>
2024-12-24 22:06:18 -0300

    Update: button size

--- components/global/buttons/PlusButton.vue
+++ components/global/buttons/PlusButton.vue
@@ -3,7 +3,7 @@ import PlusIcon from '../../icons/PlusIcon.vue';
 </script>
 
 <template>
-  <button class="rounded-full bg-primary-color transition-colors duration-200 hover:bg-primary-color/70 flex justify-center items-center w-[40px] h-[40px]">
+  <button class="rounded-full bg-primary-color transition-colors duration-200 hover:bg-primary-color/70 flex justify-center items-center w-[35px] h-[35px]">
     <PlusIcon size="15"/>
   </button>
 </template>
