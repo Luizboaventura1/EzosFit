3924b03a4e496b28ab0630a50a3c571e3fe5573a
Luiz Henrique <luizboaventura24@gmail.com>
2025-01-01 20:51:50 -0300

    New component:  NumberInputForm

---
+++ components/questionnaire/NumberInputForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="flex gap-3 text-light-gray font-medium border border-charcoal-gray bg-dark-charcoal rounded-full overflow-hidden"
+  >
+    <button
+      type="button"
+      @click="$emit('modelValue', ++inputValue)"
+      class="border-r border-charcoal-gray ps-5 pr-3 py-3 hover:bg-charcoal transition-colors duration-200"
+    >
+      <PlusIcon size="14" />
+    </button>
+    <div class="grid gap-2 grid-cols-[1fr,auto]">
+      <input
+        @input="$emit('modelValue', inputValue)"
+        v-model="inputValue"
+        class="appearance-none bg-transparent text-center focus:outline-none py-3"
+        type="number"
+      />
+      <div v-if="props.unit" class="text-medium-gray foont-light self-end pb-2">{{ props.unit  }}</div>
+    </div>
+    <button
+      type="button"
+      @click="$emit('modelValue', --inputValue)"
+      class="border-s border-charcoal-gray pr-5 ps-3 py-3 hover:bg-charcoal transition-colors duration-200"
+    >
+      <LessIcon size="14" /> 
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LessIcon from '../icons/LessIcon.vue';
+import PlusIcon from '../icons/PlusIcon.vue';
+
+const inputValue = ref(0);
+
+const props = defineProps<{
+  unit?: string
+}>()
+
+watch(inputValue, (newValue, oldValue) => {
+ if (newValue < 0) inputValue.value = oldValue
+})
+</script>
