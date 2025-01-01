e4c2c6773196b2f178f9907f7173e4c83cfbe4e4
Luiz Henrique <luizboaventura24@gmail.com>
2025-01-01 20:52:26 -0300

    New component: SecondaryButton

---
+++ components/global/buttons/SecondaryButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    type="button"
+    class="text-light-gray font-medium border border-charcoal-gray bg-dark-charcoal hover:bg-charcoal px-4 py-2 rounded-lg transition-colors duration-200"
+  >
+    <slot />
+  </button>
+</template>
