f346d0aaa4d77523e270fe59d38ebabdaf5e4077
Luiz Henrique <luizboaventura24@gmail.com>
2025-01-26 13:18:02 -0300

    feat: create privacy policy and terms page

---
+++ pages/terms.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="bg-dark-charcoal p-8 text-white">
+    <div class="flex justify-center container mx-auto px-4">
+      <div class="max-w-4xl">
+        <h1 class="text-4xl font-bold mb-6 text-center py-14">Terms</h1>
+        <p class="mb-10 text-xl text-center text-neutral-100">
+          Welcome to EzosFit. By using our software and services, you agree to
+          comply with the following terms and conditions. Please read them
+          carefully before using our app.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">1. Acceptance of Terms</h2>
+        <p class="mb-4 text-neutral-300">
+          By accessing and using EzosFit, you accept and agree to be bound by
+          these Terms and Conditions, including any future updates or
+          modifications. If you do not agree to these terms, please do not use
+          our app.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">
+          2. User Responsibilities
+        </h2>
+        <p class="mb-4 text-neutral-300">
+          As a user of EzosFit, you agree to provide accurate and truthful
+          information, and you are responsible for maintaining the
+          confidentiality of your account details. You must not engage in any
+          activity that violates the rights of others or the functionality of
+          the app.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">3. Privacy Policy</h2>
+        <p class="mb-4 text-neutral-300">
+          Your privacy is important to us. We collect and use your personal data
+          according to our
+          <NuxtLink to="/privacy-policy" class="text-blue-400"
+            >Privacy Policy</NuxtLink
+          >. By using our app, you consent to the collection and use of your
+          data as described in our Privacy Policy.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">4. Content and Usage</h2>
+        <p class="mb-4 text-neutral-300">
+          The content provided by EzosFit is for informational purposes only. We
+          do not offer medical advice or professional health services. Always
+          consult a healthcare professional before making any significant
+          dietary or fitness changes.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">
+          5. Subscription and Payments
+        </h2>
+        <p class="mb-4 text-neutral-300">
+          Some features of EzosFit require a paid subscription. Payment terms,
+          cancellation policies, and pricing can be found in the app's
+          subscription section. By subscribing, you agree to these terms.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">6. Termination</h2>
+        <p class="mb-4 text-neutral-300">
+          We reserve the right to suspend or terminate your access to EzosFit if
+          you violate these terms or engage in prohibited activities. You can
+          also terminate your account at any time by following the steps in your
+          account settings.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">
+          7. Limitation of Liability
+        </h2>
+        <p class="mb-4 text-neutral-300">
+          EzosFit and its team are not responsible for any loss or damage that
+          may occur as a result of using the app, including but not limited to
+          health-related issues, incorrect information, or any third-party links
+          or content.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">
+          8. Changes to the Terms
+        </h2>
+        <p class="mb-4 text-neutral-300">
+          We may update or modify these Terms and Conditions from time to time.
+          All changes will be posted on this page with an updated revision date.
+          Your continued use of the app after such modifications indicates your
+          acceptance of the new terms.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">9. Governing Law</h2>
+        <p class="mb-4 text-neutral-300">
+          These Terms and Conditions are governed by the laws of Brazil. Any
+          disputes arising from the use of the app will be subject to the
+          exclusive jurisdiction of the courts in Brazil.
+        </p>
+        <h2 class="text-2xl font-semibold mt-6 mb-4">10. Contact Us</h2>
+        <p class="mb-4 text-neutral-300">
+          If you have any questions or concerns about these Terms and
+          Conditions, please contact at
+          <a href="mailto:luizhenrique9x09@gmail.com" class="text-blue-400"
+            >luizhenrique9x09@gmail.com</a
+          >.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <Footer />
+</template>
+
+<script setup lang="ts">
+import Footer from "~/layouts/Footer.vue";
+</script>
+
+<style scoped></style>
